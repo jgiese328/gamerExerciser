f271b47e8e4d00ae90e7a072d6d911479291a1b1
Jason Giese <jgiese328@gmail.com>
2016-02-15 01:20:32 -0500

    Getting closer... committing most recent changes.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,99 @@
+
+/* exercises.html*/
+function imageToggle(X) {
+	$('#image'+X).click(function(){
+		$('#description'+X).toggle("fast");
+	});
+}
+/* END - exercises.html*/
+
+/* going-further.html*/
+var lvl1 =
+{
+	"1" : "Take a walk around the block, then do five pushups.",
+	"2" : "Do five pushups, five crunches, 15 second plank.",
+	"3" : "Jog around the block OR walk around the block twice.",
+	"4" : "Go for a five minute bike ride, or ride a bike at the gym/at home for five minutes. If you do not have access to a bike, play a video game with cardio, such as Dance Dance Revolution for five minutes.",
+	"5" : "Gamer day: play a game with some form of cardio element. Challenge yourself, time is up to you.",
+	"6" : "Go for a walk, pick your favorite album and continue for at least the length of four songs.",
+	"7" : "Head to the gym, do a core circuit if available. If you do not have a gym membership, do ten crunches and a 30 second plank.",
+	"8" : "Pick out a location closeby and go for a hike. If the weather is bad or you do not want to go outside, walk around the mall.",
+	"9" : "Do 10 pushups followed by 10 bicycle kicks.",
+	"10" : "Do 15 jumping jacks followed by 10 pushups.",
+	"11" : "Do 15 mountain climbers followed by 15 jumping jacks",
+	"12" : "Walk around the block twice <b>OR</b> play a cardio game for 30 minutes.",
+	"13" : "Do one set of the superman core exercise. <a href=\"http://www.wikihow.com/Perform-the-Superman-Core-Exercise\">Instructions</a>",
+	"14" : "Swim two laps OR jog around the block twice.",
+	"15" : "Go for a 10 minute bike ride, or walk a mile.",
+	"16" : "Go to the gym and walk on the elliptical for 10 minutes followed by 10 pushups. No gym membership? Walk for 15 minutes either outside or at the mall.",
+	"17" : "Go to the gym and get on the stair climber for at least 15 floors. If no gym is available, find a staircase and go up and down it 15 times.",
+	"18" : "Go out and swim three laps, otherwise, take a hike! If either of those are not an option, pick your favorite cardio video game and exercise for at least ten minutes",
+	"19" : "Take out your favorite exercise video game and go to town! Work out for at least 15 minutes.",
+	"20" : "Do 15 pushups and 15 crunches. Bonus, if you beat a boss in a game you are playing, take some time out to walk around the block.",
+	"21" : "Go to the gym, check out the core exercise area and work out on at least three machines for 10 reps each.",
+	"22" : "Walk around the block twice, do two sets of 30 second planks.",
+	"23" : "Jog around the block if you are able, otherwise walk around the block twice.",
+	"24" : "Do 10 pushups, 15 jumping jacks, and 20 mountain climbers.",
+	"25" : "Do one set of the superman core exercise. <a style=\"color:black;\" href=\"http://www.wikihow.com/Perform-the-Superman-Core-Exercise\">Instructions</a>",
+	"26" : "Cardio game day, work out at least for 10 minutes to your favorite workout game. If you have a dance game, get down until you break a sweat!",
+	"27" : "Take your bike out for 15 minutes or go walk around a hiking trail. Finish up with 10 pushups.",
+	"28" : "Get to the gym, choose your cardio machine of choice and get to it. Do at least 15 minutes. If you do not have access to a gym, go out for a walk/jog.",
+	"29" : "Do two sets of each: 10 pushups, 30 second plank, 10 mountain climbers, 10 jumping jacks.",
+	"30" : "Cardio game day: choose a game of your choice with cardio elements and exercise for at least 10 minutes.",
+	"31" : "Open-ended day. Choose either to exercise with a video game that incorporates cardio, go out for a jog, go biking, go to the gym, or walk around at the mall. Your choice, do at least 10 minutes."
+}
+
+var lvl2 =
+{
+	"1" : "Take a jog around the block, then do 10 pushups.",
+	"2" : "Do ten pushups, ten crunches, and a 30 second plank.",
+	"3" : "Jog around the block OR walk around the block three times.",
+	"4" : "Go for a ten minute bike ride, or ride a bike at the gym/at home for ten minutes. If you do not have access to a bike, play a video game with cardio, such as Dance Dance Revolution for ten minutes.",
+	"5" : "Gamer day: play a game with some form of cardio element. Challenge yourself, time is up to you. Mix in three sets of 10 pushups during your workout.",
+	"6" : "Go for a walk, pick your favorite album and continue for at least the length of six songs.",
+	"7" : "Head to the gym, do a core circuit if available. If you do not have a gym membership, do twenty crunches and two 30 second planks.",
+	"8" : "Pick out a location close by and go for a hike. If the weather is bad or you do not want to go outside, walk around the mall. Add at least one set of 15 crunches",
+	"9" : "Do 20 pushups followed by 20 bicycle kicks.",
+	"10" : "Do 25 jumping jacks followed by 15 pushups.",
+	"11" : "Do 30 mountain climbers followed by 30 jumping jacks",
+	"12" : "Jog around the block twice <b>OR</b> play a cardio game for 30 minutes.",
+	"13" : "Do two sets of the superman core exercise. <a href=\"http://www.wikihow.com/Perform-the-Superman-Core-Exercise\">Instructions</a>",
+	"14" : "Swim three laps OR jog around the block three times.",
+	"15" : "Go for a 15 minute bike ride, or jog a mile.",
+	"16" : "Go to the gym and walk on the elliptical for 15 minutes followed by 15 pushups. No gym membership? Walk for 20 minutes either outside or at the mall.",
+	"17" : "Go to the gym and get on the stair climber for at least 20 floors. If no gym is available, find a staircase and go up and down it 20 times.",
+	"18" : "Go out and swim four laps, otherwise, take a hike! If either of those are not an option, pick your favorite cardio video game and exercise for at least twenty minutes",
+	"19" : "Take out your favorite exercise video game and go to town! Work out for at least 25 minutes.",
+	"20" : "Do 25 pushups and 25 crunches. Bonus, if you beat a boss in a game you are playing, take some time out to walk around the block.",
+	"21" : "Go to the gym, check out the core exercise area and work out on at least three machines for 15 reps each.",
+	"22" : "Walk or jog around the block three times, do two sets of 30 second planks.",
+	"23" : "Jog around the block four times if you are able, otherwise walk around the block four times.",
+	"24" : "Do 15 pushups, 30 jumping jacks, and 30 mountain climbers.",
+	"25" : "Do two sets of the superman core exercise. <a style=\"color:black\" href=\"http://www.wikihow.com/Perform-the-Superman-Core-Exercise\">Instructions</a>",
+	"26" : "Cardio game day, work out at least for 30 minutes to your favorite workout game. If you have a dance game, get down until you break a sweat!",
+	"27" : "Take your bike out for 25 minutes or go walk around a hiking trail. Finish up with 15 pushups.",
+	"28" : "Get to the gym, choose your cardio machine of choice and get to it. Do at least 25 minutes. If you do not have access to a gym, go out for a walk/jog.",
+	"29" : "Do three sets of each: 15 pushups, 30 second plank, 15 mountain climbers, 15 jumping jacks.",
+	"30" : "Cardio game day: choose a game of your choice with cardio elements and exercise for at least 20 minutes.",
+	"31" : "Open-ended day. Choose either to exercise with a video game that incorporates cardio, go out for a jog, go biking, go to the gym, or walk around at the mall. Your choice, do at least 20 minutes."
+}
+
 var today = new Date().getUTCDate();
+
+var challenge = "<h3>If you choose to accept it:</h3>";
+
+
+$("#generate").click(function() {
+
+	if($("#skillLevel").val() == 'lvl1')
+	{
+		var replacement = lvl1[today];
+	}
+	if($("#skillLevel").val() == 'lvl2')
+	{
+		var replacement = lvl2[today];
+	}
+
+	$(".challengeReplace").html(challenge+'<p class="challengeP">'+replacement+'</p>');
+	});
+/* END - going-further.html*/
